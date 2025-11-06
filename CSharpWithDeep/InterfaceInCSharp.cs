e79c30d7998205bb1f5f6d98826b85ae6dc51bed
Divyanshu Rai <raidivyanshu19032000@gmail.com>
2025-11-06 22:04:20 +0530

    Working with params

---
+++ CSharpWithDeep/InterfaceInCSharp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpWithDeep
+{
+    //Interface in C#
+    // 1- Whenever you want to create a interface you will have to use interface keyword to create it.
+    // 2- Always keep 'I' as a prefix with the interface as a naming convention.
+    // 3- It's contract which other types (Class which is using this interface) will have to follow that contract.
+    // 4 - All the interface members will be by default public.
+    class InterfaceInCSharp
+    {
+       
+    }
+}
--- CSharpWithDeep/WorkingWithParameter.cs
+++ CSharpWithDeep/WorkingWithParameter.cs
@@ -19,10 +19,49 @@ namespace CSharpWithDeep
         // That means any changes made insdie the method do not affect the original variable.
         int PassByValue(int x, int y)
         {
-            x  = x + 10;
+            x = x + 10;
             y = x + 40;
             return x + y;
         }
+
+        // 2. Pass by ref
+        // When a variable is passed by reference the memory address of the variable is sent to the method. 
+        // That means the method works on the original data not a copy.
+        // In case of the pass by ref you need to pass value along with the ref.
+
+        //Note : 
+        // In case of pass by value : The actuall value is not comming on params only copy of that particular 
+        // value is comming. (That means the actual value will not change here).
+        //In case of pass by ref : The actual value is passing through the parms. It means the actaul value will 
+        // also change in the case of pass by ref.
+        int PassByRef(ref int x, ref int y)
+        {
+            x = x + 10;
+            y = x + 40;
+            return x + y;
+        }
+
+
+        // 3- Pass by out 
+        // When you use the out keyword the variable is passed by reference, just like ref, but with one key
+        // difference: 
+        // You don't need to initialize the variable before passing it. 
+        // The called method must assing a value to it before returing. 
+
+        int PassByOut(int x, int y, out int a)
+        {
+            x = x + 10;
+            y = x + 40; 
+            a = x + y + 50;
+            return x + y;
+        }
+
+        //4- Pass by Param
+
+        //int AddNumbers(int a, int b, int c, int d, int e)
+        //{
+        //    return a + b + c;
+        //}
         static void Main()
         {
             int i = 10, j = 20;
@@ -31,8 +70,24 @@ namespace CSharpWithDeep
 
             //--------------------------- Pass by Value Example -----------------------------
 
-            Console.WriteLine("Pass by value result is : "+ obj.PassByValue(i, j));
-            Console.WriteLine("I value is : "+ i + "\n" +  " J value is : "+ j );
+            //Console.WriteLine("Pass by value result is : " + obj.PassByValue(i, j));
+            //Console.WriteLine("I value is : " + i + "\n" + " J value is : " + j);
+
+
+            //-------------------------- Pass by Ref example ---------------------------
+            //Console.WriteLine("Pass by ref example start here..");
+            //Console.WriteLine("Pass by ref result is : " + obj.PassByRef(ref i, ref j));
+            //Console.WriteLine(i);
+            //Console.WriteLine(j);
+
+
+
+            //-------------------------- Pass by Out example ---------------------------
+            //Console.WriteLine("Pass by ref example start here..");
+            //Console.WriteLine("Pass by out result is : "+ obj.PassByOut(i, j, out int a));  //80
+            //Console.WriteLine(i);
+            //Console.WriteLine(j); 
+            //Console.WriteLine(a); 
             Console.ReadKey();
         }
     }
