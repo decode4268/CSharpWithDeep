23c25d1ea053ef6c26069a4bebd3612155d8546f
Divyanshu Rai <raidivyanshu19032000@gmail.com>
2025-11-13 21:58:30 +0530

    Exception Handling in C#

--- CSharpWithDeep/ExceptionHandlingInCSharp.cs
+++ CSharpWithDeep/ExceptionHandlingInCSharp.cs
@@ -11,12 +11,42 @@ namespace CSharpWithDeep
         static void Main()
         {
             // Exception Handling in C#
-            //int i = 100;
+            int i = 100;
             //int result = i / 0;
 
-            int[] myValue = new int[1];
-            myValue[1] = 100;
-            Console.WriteLine("Result is : "+ myValue[1]);
+            //int[] myValue = new int[1];
+            //myValue[1] = 100;
+            //Console.WriteLine("Result is : "+ myValue[1]);
+
+
+            int result;
+          
+            try
+            {
+                // Here you will keep piece of code which you assume 
+                // error occure could be.
+                result = i / 0;
+                Console.WriteLine("Hey, I am visible here because there is no error.");
+
+            }
+            catch (Exception ex)
+            {
+                //If an error will be occured into try section then only this block will be
+                // executed
+                //Console.WriteLine(ex.Message);
+
+                Console.WriteLine("Message:" + ex.Message + " StackTrace:"+ ex.StackTrace
+                    + "Source:"+ ex.Source + "TargetSite: "+ex.TargetSite.Name + " DateTime :"+ DateTime.Now);
+
+                //throw;
+
+            }
+            finally
+            {
+                // This block be always executed.
+                Console.WriteLine("This is the finally block");
+            }
+
             Console.ReadKey();
         }
     }
---
+++ CSharpWithDeep/MultipleCatchBlock.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpWithDeep
+{
+    public class MultipleCatchBlock
+    {
+        static void Main()
+        {
+            try
+            {
+                int a = 100;
+                int[] arr = { 1, 2, 3 };
+                Console.WriteLine(arr[2]); // Index out of Range Exception.
+
+                int result = a / 0;
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine("You try to divide by zero");
+            }
+            catch (IndexOutOfRangeException ex)
+            {
+                Console.WriteLine("Array index is out of range");
+                Console.WriteLine("Error: " + ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("General Exception " + ex.Message);
+            }
+            Console.ReadKey();
+        }
+    }
+}
