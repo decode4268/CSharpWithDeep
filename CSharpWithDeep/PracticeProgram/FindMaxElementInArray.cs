1172e84049a307d0d0425fe96ecbbe539be1bcb4
Divyanshu Rai <raidivyanshu19032000@gmail.com>
2025-11-15 23:01:11 +0530

    Array Program and Properties in C#.

---
+++ CSharpWithDeep/PracticeProgram/FindMaxElementInArray.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpWithDeep.PracticeProgram
+{
+    public class FindMaxElementInArray
+    {
+        static void Main(string[] args)
+        {
+            int[,] arr =
+            {
+                {1,4,7 },
+                {2,5,7 },
+                { 4,7,8}
+            };
+            int highest = arr[0, 0];
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                for (int j = 0; j < arr.GetLength(1); j++)
+                {
+                    if (arr[i, j] > highest)
+                        highest = arr[i, j];
+                }
+            }
+            Console.WriteLine("Highest element : "+ highest);
+        }
+    }
+}
---
+++ CSharpWithDeep/Properties.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpWithDeep
+{
+    // A properties is a type member that provided a way to read, write private field.
+    public class Properties
+    {
+        int magicNumber;
+        public int GetOrSetValue
+        {
+            get
+            {
+                return magicNumber;
+            }
+            set
+            {
+                magicNumber = value;
+            }
+        }
+
+        // for setting the value of magicNumber
+        public void SetValue(int number)
+        {
+            magicNumber = number;
+        }
+
+        // for getting the value of magicNumber
+        public int GetValue()
+        {
+            return magicNumber;
+        }
+    }
+    public class DeepRaj
+    {
+        static void Main()
+        {
+            var obj = new Properties();
+            //obj.magicNumber;
+            //obj.SetValue(100);
+            //Console.WriteLine(obj.GetValue());
+
+            obj.GetOrSetValue = 200;
+            Console.WriteLine(obj.GetOrSetValue);
+
+            Console.ReadKey();
+        }
+    }
+}
