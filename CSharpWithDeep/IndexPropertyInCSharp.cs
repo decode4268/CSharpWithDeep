cb3bc27d037c99321fc85c11b95a7ecf3027102c
Divyanshu Rai <raidivyanshu19032000@gmail.com>
2025-11-16 20:59:58 +0530

    IndexerProperties.

---
+++ CSharpWithDeep/IndexPropertyInCSharp.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpWithDeep
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+    public class Company
+    {
+        List<Employee> empList;
+        public Company()
+        {
+            empList = new List<Employee>()
+            {
+                new Employee() {Id = 1, Name = "David", Age = 35},
+                new Employee() {Id = 2, Name = "DeepRaj", Age = 25},
+                new Employee() {Id = 3, Name = "Govind", Age = 27},
+                new Employee() {Id = 4, Name = "Divyanshu", Age = 26},
+            };
+        }
+
+        // Id = 1, Name = "David" // Get
+        // Id = 1, Name = "David Test " // Set
+
+        // Indexer Properties
+        public string this[int Id]
+        {
+            get
+            {
+                //return empList.Where(e => e.Id == Id).FirstOrDefault().Name;
+                return empList.FirstOrDefault(e => e.Id == Id).Name;
+            }
+            set
+            {
+                empList.FirstOrDefault(e => e.Id == Id).Name = value;
+            }
+        }
+
+        static void Main()
+        {
+            var obj = new Company();     // constructor has called on obj creation of the class.
+
+        
+            obj[1] = "David Test";
+            Console.WriteLine(obj[1]);
+            Console.ReadKey();
+        }
+    }
+}
