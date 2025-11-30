d6fbac1e58eb39409e5a884e28da947e797d2d46
Divyanshu Rai <raidivyanshu19032000@gmail.com>
2025-11-30 22:26:58 +0530

    Collection Crud Part 2

--- CSharpWithDeep/CollectionCurd/CollectionCurdAppls.cs
+++ CSharpWithDeep/CollectionCurd/CollectionCurdAppls.cs
@@ -8,16 +8,6 @@ using System.Xml.Serialization;
 
 namespace CSharpWithDeep.CollectionCurd
 {
-    enum UserChoice
-    {
-        AddDetails = 1,
-        ShowAllDetails = 2,
-        SingleDetails = 3,
-        UpdateDetails = 4,
-        DeleteDetails = 5,
-        Exit = 6
-    }
-
     // CURD => Create, Update, Read, Delete
     public class CollectionCurdAppls
     {
@@ -28,34 +18,48 @@ namespace CSharpWithDeep.CollectionCurd
         }
         static void Main()
         {
-            Console.WriteLine("Please enter your Choice \n 1-Add Details, \n 2-Show All Details \n " +
-                "3-Single Details, \n 4-Update Details \n 5-Delete Details \n 6-Exit");
-
-            int userInput = Convert.ToInt32(Console.ReadLine());
-
-            UserChoice userChoice = (UserChoice)userInput;
-
-            switch (userChoice)
+            bool isItOk = true;
+            while (isItOk)
             {
-                case UserChoice.AddDetails:
-                    var employeeDetails = UserInfoForAdd();
-                    Console.WriteLine(SaveEmployeeDetails(employeeDetails));
-                    break;
-                case UserChoice.ShowAllDetails:
-                    ShowAllEmployeeDetails();
-                    break;
-                case UserChoice.SingleDetails:
-                    break;
-                case UserChoice.UpdateDetails:
-                    break;
-                case UserChoice.DeleteDetails:
-                    break;
-                case UserChoice.Exit:
-                    break;
-                default:
-                    Console.WriteLine("your Choice is wrong");
-                    break;
+                Console.WriteLine("Please enter your Choice \n 1-Add Details, \n 2-Show All Details \n " +
+               "3-Single Details, \n 4-Update Details \n 5-Delete Details \n 6-Exit");
+
+                int userInput = Convert.ToInt32(Console.ReadLine());
+
+                UserChoice userChoice = (UserChoice)userInput;
+
+                switch (userChoice)
+                {
+                    case UserChoice.AddDetails:
+                        var employeeDetails = UserInfoForAdd();
+                        Console.WriteLine(SaveEmployeeDetails(employeeDetails));
+                        Console.WriteLine();
+                        Console.WriteLine("---------------");
+                        break;
+                    case UserChoice.ShowAllDetails:
+                        ShowAllEmployeeDetails();
+                        Console.WriteLine();
+                        Console.WriteLine("---------------");
+                        break;
+                    case UserChoice.SingleDetails:
+                        Console.WriteLine(FindSingleEmployeeDetail());
+                        Console.WriteLine();
+                        Console.WriteLine("---------------");
+                        break;
+                    case UserChoice.UpdateDetails:
+                        break;
+                    case UserChoice.DeleteDetails:
+                        break;
+                    case UserChoice.Exit:
+                        isItOk = false;
+                        Console.WriteLine("Than you for visiting.");
+                        break;
+                    default:
+                        Console.WriteLine("your Choice is wrong");
+                        break;
+                }
             }
+
             Console.ReadKey();
         }
 
@@ -80,6 +84,18 @@ namespace CSharpWithDeep.CollectionCurd
 
         static string SaveEmployeeDetails(Employee employee)
         {
+            if (!string.IsNullOrEmpty(employee.EmailId))
+            {
+                if (IsEmailExist(employee.EmailId))
+                {
+                    return "Oops! Email Id already exist. Pleae try again with other email Id";
+                }
+            }
+            else
+            {
+                Console.WriteLine("Kindly enter a email, Email is required");
+            }
+
             int maxId = 0;
             foreach (var empInfo in employessList)
             {
@@ -88,14 +104,14 @@ namespace CSharpWithDeep.CollectionCurd
                     maxId = empInfo.Id;
                 }
             }
-            employee.Id = maxId == 0 ? 1 : maxId + 1; 
+            employee.Id = maxId == 0 ? 1 : maxId + 1;
             employessList.Add(employee);
             return "Record successfully Save.";
         }
 
         static void ShowAllEmployeeDetails()
         {
-            Console.WriteLine("----------- Please find below all employee Details");
+            Console.WriteLine("----------- Please find below all employee Details----------");
             foreach (var emp in employessList)
             {
                 Console.WriteLine("Name: " + emp.Name);
@@ -104,6 +120,82 @@ namespace CSharpWithDeep.CollectionCurd
                 Console.WriteLine("City: " + emp.City);
             }
         }
+
+        static string FindSingleEmployeeDetail()
+        {
+            string message = string.Empty;
+            Console.WriteLine("Please enter email id");
+            string email = Console.ReadLine();
+            var employee = SingleEmployeesDetail(email);
+            if (employee != null)
+            {
+                Console.WriteLine("-------------Employee Detail------------------");
+                Console.WriteLine("Name: " + employee.Name);
+                Console.WriteLine("Age: " + employee.Age);
+                Console.WriteLine("Email: " + employee.EmailId);
+                Console.WriteLine("City: " + employee.City);
+            }
+            else
+            {
+                message = "We did not find any records with this email id";
+            }
+            return message;
+        }
+
+        static Employee SingleEmployeesDetail(string email)
+        {
+            var employeeFromList = employessList.
+                FirstOrDefault(e => e.EmailId.ToLower().Trim() == email.ToLower().Trim());
+            if (employessList != null)
+            {
+                return employeeFromList;
+            }
+            return null;
+        }
+
+        static string GetEmployeeDetailsForUpdate()
+        {
+            string message = string.Empty;
+            Console.WriteLine("Please enter email id");
+            string emailId = Console.ReadLine();
+            if (!string.IsNullOrEmpty(emailId) && IsEmailExist(emailId))
+            {
+                var employee = new Employee();
+                Console.WriteLine("Please enter name");
+                employee.Name = Console.ReadLine();
+                Console.WriteLine("Please Enter the age");
+                employee.Age = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Please enter the city");
+                employee.City = Console.ReadLine();
+            }
+            else
+            {
+                message = "Email id does not exist into our records";
+            }
+            return message; 
+        }
+
+        #region CommonHelper Method
+        static bool IsEmailExist(string emailId)
+        {
+            bool isEmailExist = false;
+            if (!string.IsNullOrEmpty(emailId))
+            {
+                var employee = employessList.
+                    FirstOrDefault(e => e.EmailId.ToLower().Trim() == emailId.ToLower().Trim());
+                if (employee == null)
+                {
+                    isEmailExist = false;
+                }
+                else
+                {
+                    isEmailExist = true;
+                }
+            }
+
+            return isEmailExist;
+        }
+        #endregion
 
     }
 }
---
+++ CSharpWithDeep/CollectionCurd/UserChoice.cs
@@ -0,0 +1,12 @@
+﻿namespace CSharpWithDeep.CollectionCurd
+{
+    enum UserChoice
+    {
+        AddDetails = 1,
+        ShowAllDetails = 2,
+        SingleDetails = 3,
+        UpdateDetails = 4,
+        DeleteDetails = 5,
+        Exit = 6
+    }
+}
